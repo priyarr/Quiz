8a08688d888295ad5c620d4292ba8fc62a698720
Priya Ramcharan <priya.ramcharan@gmail.com>
2019-12-03 15:23:19 +0100

    Added methods to choose a question based on userinput for a difficulty

--- Week3OpdrDeel1/Program.cs
+++ Week3OpdrDeel1/Program.cs
@@ -12,52 +12,81 @@ namespace Week3OpdrDeel1
             {
                 Text = "Who is the inventor of C#?",
                 Answer = "Microsoft",
-                Difficulty = 2
+                Difficulty = 4
 
             };
 
+            var SecondQuestion = new Question()
+            {
+                Text = "What day comes after monday?",
+                Answer = "Tuesday",
+                Difficulty = 1
+            };
 
-            var MQuestion = new MupltipleChoiceQuestion()
+            var ThirdQuestion = new Question()
+            {
+                Text = "Is lo-fi a music genre or a movie genre?",
+                Answer = "Music genre",
+                Difficulty = 3
+            };
+
+            var FourthQuestion = new Question()
+            {
+                Text = "Who discovered America",
+                Answer = "Colombus",
+                Difficulty = 2,
+            };
+
+            var MQuestion1 = new MupltipleChoiceQuestion()
             {
                 Text = "In which country was the inventor of C# born?",
                 Answer = "America",
-                Difficulty = 1
+                Difficulty = 4
                 
             };
-
             var Choice1 = new Choice() { Answer = true, Option = "America" };
             var Choice2 = new Choice() { Answer = false, Option = "India" };
             var Choice3 = new Choice() { Answer = false, Option = "Spain" };
+            MQuestion1.Choices = new List<Choice> { Choice1, Choice2, Choice3 };
 
-            MQuestion.Choices = new List<Choice> { Choice1, Choice2, Choice3 };
+            var MQuestion2 = new MupltipleChoiceQuestion()
+            {
+                Text = "Which one is a vegatable?",
+                Answer = "Carrot",
+                Difficulty = 1
+
+            };
+            var Q2Choice1 = new Choice() { Answer = true, Option = "Carrot" };
+            var Q2Choice2 = new Choice() { Answer = false, Option = "Tomtato" };
+            var Q2Choice3 = new Choice() { Answer = false, Option = "Apple" };
+            MQuestion2.Choices = new List<Choice> { Q2Choice1, Q2Choice2, Q2Choice3 };
 
             var AllQuestions = new List<IQuestion>()
             {
                 FirstQuesiton,
-                MQuestion,
+                SecondQuestion,
+                ThirdQuestion,
+                FourthQuestion,
+                MQuestion1,
+                MQuestion2
             };
 
-            foreach(IQuestion q in AllQuestions)
-            {
-                Console.WriteLine(q.Text);
-            }
-            
-            var SortedList = SortQuestionsOnDifficulty(AllQuestions);
+            PresentQuestionOnDifficulty(AllQuestions);
 
-            foreach (IQuestion q in SortedList)
-            {
-                Console.WriteLine(q.Text);
-            }
-            Console.ReadKey();
-
-
-            //PresentQuestion(MQuestion);
-            //PresentQuestion(FirstQuesiton);
         }
 
         public static IEnumerable<IQuestion> SortQuestionsOnDifficulty(List<IQuestion> questions)
         {
             return questions.OrderBy(q => q.Difficulty);
+        }
+
+        public static IQuestion GetQuestionByDifficulty(IEnumerable<IQuestion> questions, int difficulty)
+        {
+            var questionsWithChosenDifficulty = questions.Where(q => q.Difficulty.Equals(difficulty));
+            int amount = questionsWithChosenDifficulty.Count();
+            Random r = new Random();
+            int RandomIndex = r.Next(0, amount);
+            return questionsWithChosenDifficulty.ElementAt(RandomIndex);
         }
 
         public static void PresentQuestion(IQuestion q)
@@ -68,6 +97,21 @@ namespace Week3OpdrDeel1
             Console.WriteLine("That is " + CheckAnswer(q, Answer));
             Console.ReadKey();
 
+
+        }
+
+        public static void PresentQuestionOnDifficulty(IEnumerable<IQuestion> questions)
+        {
+            Console.WriteLine("Choose a difficulty: ");
+            int ChosenDifficulty = int.Parse(Console.ReadLine());
+
+            IQuestion question = GetQuestionByDifficulty(questions, ChosenDifficulty);
+            question.Print();
+            Console.WriteLine("Your answer: ");
+            string Answer = Console.ReadLine();
+            Console.WriteLine("That is " + CheckAnswer(question, Answer));
+
+            Console.ReadKey();
 
         }
 
